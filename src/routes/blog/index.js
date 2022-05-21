da1b606f9e57ac283b580a9f835fd76047be5c46
Atreya Choudhury <atreyachoudhury@hotmail.com>
2022-05-22 00:31:03 +0800

    better blog and gallery placeholders

--- src/routes/blog/index.js
+++ src/routes/blog/index.js
@@ -1,10 +1,22 @@
 import { useEffect } from "react"
 import Container from "@mui/material/Container"
+import FullHeight from "../../components/fullHeight"
+import Typography from "@mui/material/Typography"
 
 export default function Blog() {
 	useEffect(() => {
 		document.title = "Blog - atchox"
 	})
 
-	return <Container>Blog is empty(for now)</Container>
+	return (
+		<FullHeight minMode sx={{ bgcolor: "common.black", color: "grey.300" }}>
+			<Container sx={{ pt: 6 }}>
+				<Typography variant="body1">
+					Blog is empty
+					<br />
+					for now...
+				</Typography>
+			</Container>
+		</FullHeight>
+	)
 }
--- src/routes/gallery/index.js
+++ src/routes/gallery/index.js
@@ -1,10 +1,18 @@
 import { useEffect } from "react"
 import Container from "@mui/material/Container"
+import FullHeight from "../../components/fullHeight"
+import Typography from "@mui/material/Typography"
 
 export default function Home() {
 	useEffect(() => {
 		document.title = "Gallery - atchox"
 	})
 
-	return <Container>Gallery...</Container>
+	return (
+		<FullHeight minMode sx={{ bgcolor: "common.black", color: "grey.300" }}>
+			<Container sx={{ pt: 6 }}>
+				<Typography variant="body1">Will take me a while to organise the mess that is my gallery lol {":)"}</Typography>
+			</Container>
+		</FullHeight>
+	)
 }
